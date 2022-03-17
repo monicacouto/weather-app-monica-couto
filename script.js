49fb43ddc69dba42fb6b6e3804d2b0d769eccaf8
monicacouto <monicacoutoc@gmail.com>
2022-03-17 13:04:34 +0000

    Added relevant icon from the API

--- script.js
+++ script.js
@@ -33,6 +33,13 @@ function weatherCondition(response) {
   );
   document.querySelector("#description").innerHTML =
     response.data.weather[0].main;
+  let iconElement = document.querySelector("#icon");
+
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 function searchCity(city) {
